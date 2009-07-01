36fe7b87c4c7f65ef74a3002e2aa92100a321210
lexayo <lexayo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-01 20:20:39 +0000

    no more shit after comment form
    git-svn-id: https://plugins.svn.wordpress.org/1silex4wp/trunk@131296 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- framed_theme/footer.php
+++ framed_theme/footer.php
@@ -7,9 +7,9 @@
 <!-- begin footer -->
 </div>
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 
-<p class="credit"><!--<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. --> <cite><?php echo sprintf(__("Powered by <a href='http://wordpress.org/' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?></cite></p>
+<p><!--<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. --> <cite><?php echo sprintf(__("Powered by <a href='http://wordpress.org/' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?></cite></p>
 
 </div>
 
