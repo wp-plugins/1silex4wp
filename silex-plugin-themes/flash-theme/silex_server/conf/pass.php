ed438b7a2b8d22a6da7f4b155873374de3b2601a
lexayo <lexayo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-25 16:50:51 +0000

    git-svn-id: https://plugins.svn.wordpress.org/1silex4wp/trunk@139212 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- silex-plugin-themes/flash-theme/silex_server/conf/pass.php
+++
@@ -1,7 +0,0 @@
-<?php
-$authentication['LOGINS']['a'] = 'a';
-$authentication['LOGINS']['aa'] = 'aa';
-$authentication['LOGINS']['x'] = 'x';
-$authentication['LOGINS']['ww'] = 'ww';
-$authentication['LOGINS']['jj'] = 'jj';
-?>
